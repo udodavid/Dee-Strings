25432b10d41f0da34ad913baad467ec28497df4d
udodavid <davidudo00@gmail.com>
2026-01-18 17:48:36 +0000

    js

--- script/script.js
+++ script/script.js
@@ -274,3 +274,20 @@ form.addEventListener("submit", async (e) => {
     // Redirect
     window.location.href = "success.html";
 });
+// Success
+const handleSubmit = event => {
+  event.preventDefault();
+
+  const myForm = event.target;
+  const formData = new FormData(myForm);
+
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString()
+  })
+    .then(() => alert("Thank you for your submission"))
+    .catch(error => alert(error));
+};
+
+document.querySelector("form").addEventListener("submit", handleSubmit);
